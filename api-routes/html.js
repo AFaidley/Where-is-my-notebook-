6d339b41516fad762fc48edf57380a0ab4fc09c0
AFaidley <alviva.faidley@gmail.com>
2022-08-30 13:33:17 -0600

    Added content to api route, html.

---
+++ api-routes/html.js
@@ -0,0 +1,21 @@
+// Global vars and required packages
+const path = require('path');
+const router = require('express').Router();
+
+// This will set home (/) to the index.html page
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// GET route for notes.html page
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Catch all route to redirect to homepage- index.html as requested in the ReadMe
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// export router 
+module.exports = router;
